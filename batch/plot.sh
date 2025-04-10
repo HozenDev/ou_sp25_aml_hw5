1e7f0a176ed459f34c008c770ef766f51f5e0141
Hozen <enzo.durel@gmail.com>
2025-04-09 20:26:08 -0500

    plot

--- batch/plot.sh
+++ batch/plot.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu
-#SBATCH -x c860
-#SBATCH --gres=gpu:1
+#SBATCH --partition=normal
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
-#SBATCH --mem=20G
-#SBATCH --output=results/plot_%j_stdout.txt
-#SBATCH --error=results/plot_%j_stderr.txt
-#SBATCH --time=00:20:00
-#SBATCH --job-name=aml_plot
+#SBATCH --mem=1G
+#SBATCH --output=results/plot/%j_stdout.txt
+#SBATCH --error=results/plot/%j_stderr.txt
+#SBATCH --time=00:10:00
+#SBATCH --job-name=plot
 #SBATCH --mail-user=Enzo.B.Durel-1@ou.edu
 #SBATCH --mail-type=ALL
-#SBATCH --chdir=/home/cs504305/hw4/code/
+#SBATCH --chdir=/home/cs504305/hw5
 
 #################################################
 # Do not change this line unless you have your own python/tensorflow/keras set up
 . /home/fagg/tf_setup.sh
 conda activate dnn
 
-python plot.py -v @exp.txt @oscer.txt
+CODE_DIR=code
+CONFIG_DIR=configs
+
+## SHALLOW
+python ${CODE_DIR}/plot.py \
+       @${CONFIG_DIR}/exp.txt @${CONFIG_DIR}/net.txt \
+       --results_path "./results/exp/" \
